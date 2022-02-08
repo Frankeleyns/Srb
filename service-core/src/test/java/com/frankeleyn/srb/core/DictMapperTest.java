2e9dec328c4701ebe5db4664fcdbbdf3d458cad2
frankeleyn <2582726641@qq.com>
2022-02-08 19:22:37 +0800

    添加数据字典 img

--- service-core/src/test/java/com/frankeleyn/srb/core/DictMapperTest.java
+++ service-core/src/test/java/com/frankeleyn/srb/core/DictMapperTest.java
@@ -1,20 +1,19 @@
 package com.frankeleyn.srb.core;
 
 import com.frankeleyn.srb.core.mapper.DictMapper;
-import com.frankeleyn.srb.core.pojo.dto.ExcelDictDTO;
 import com.frankeleyn.srb.core.service.DictService;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.test.context.junit4.SpringRunner;
 
 /**
  * @author Frankeleyn
  * @date 2022/2/8 10:39
  */
 @SpringBootTest
+@RunWith(SpringRunner.class)
 public class DictMapperTest {
 
     @Autowired
@@ -22,13 +21,6 @@ public class DictMapperTest {
 
     @Autowired
     private DictService dictService;
-
-    @Test
-    public void testInsert() {
-        List<ExcelDictDTO> list = new ArrayList<>();;
-        list.add(new ExcelDictDTO(1L, 0L, "ROOT", 1, "ROOT"));
-        dictMapper.insertBatch(list);
-    }
 
     @Test
     public void testDictService() {
